77bfc51b5219f6906079772ad05571cd9bb301dc
Hamza Bashir <hamzabashir218@gmail.com>
2024-11-28 20:03:26 +0500

    user setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+  
+    validates :email, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
+  end
+  
